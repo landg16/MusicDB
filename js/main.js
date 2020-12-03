3a9f4d2244bb83e21e89b449a3fe6b5a82d7d3b6
Lasha Andghuladze <landg16@freeuni.edu.ge>
2020-12-03 19:28:07 +0400

    Added dummy data loading into homepage.

--- js/main.js
+++ js/main.js
@@ -9,26 +9,59 @@ const router = new Router({
 })
 
 const index = function() {
-    fetch("./template/main.html")
-        .then(response => {
-            return response.text()
+    fetchPage("./template/main.html", function(data) {
+        document.getElementById("page").innerHTML = data
+
+        // Add data to featured artists
+        let slideTemplate = document.querySelector('#slide');
+        getData("./data/artists.json", function(data) {
+            data = data.data
+            shuffleArray(data)
+            for (let i = 0; i < data.length / 2; i++) {
+                let clone = slideTemplate.content.cloneNode(true)
+                clone.querySelector('.img').style.backgroundImage = "url('img/artists/" + data[i].img + "')"
+                clone.querySelector('h4').style.marginBottom = "10px"
+                clone.querySelector('h4').textContent = data[i].name
+                clone.querySelector('p').style.display = "none"
+                document.getElementById('featuredArtists').getElementsByClassName('carousel-row')[0].appendChild(clone)
+            }
         })
-        .then(data => {
-            document.getElementById("page").innerHTML = data;
-            slider('featuredArtists')
-            slider('featuredAlbums')
-            slider('featuredTracks')
-        });
+
+        slider('featuredArtists')
+
+        getData("./data/albums.json", function(data) {
+            data = data.data
+            shuffleArray(data)
+            for (let i = 0; i < data.length / 2; i++) {
+                let clone = slideTemplate.content.cloneNode(true)
+                clone.querySelector('.img').style.backgroundImage = "url('img/albums/" + data[i].img + "')"
+                clone.querySelector('h4').textContent = data[i].album_name
+                clone.querySelector('p').textContent = data[i].artist_name
+                document.getElementById('featuredAlbums').getElementsByClassName('carousel-row')[0].appendChild(clone)
+            }
+        })
+
+        slider('featuredAlbums')
+
+        getData("./data/tracks.json", function(data) {
+            data = data.data
+            shuffleArray(data)
+            for (let i = 0; i < data.length / 2; i++) {
+                let clone = slideTemplate.content.cloneNode(true)
+                clone.querySelector('.img').style.backgroundImage = "url('img/albums/" + data[i].img + "')"
+                clone.querySelector('h4').textContent = data[i].track_name
+                clone.querySelector('p').textContent = data[i].artist_name + " - " + data[i].album_name
+                document.getElementById('featuredTracks').getElementsByClassName('carousel-row')[0].appendChild(clone)
+            }
+        })
+        slider('featuredTracks')
+    });
 }
 
 const about = function() {
-    fetch("./template/about.html")
-        .then(response => {
-            return response.text()
-        })
-        .then(data => {
-            document.getElementById("page").innerHTML = data;
-        });
+    fetchPage("./template/about.html", function(data) {
+        document.getElementById("page").innerHTML = data;
+    });
 }
 
 router
@@ -37,10 +70,8 @@ router
 
 window.onload = function() {
     const navigation = document.getElementsByClassName('nav-link')
-    console.log(navigation)
     for(let i = 0; i < navigation.length; i++) {
         navigation[i].onclick = function() {
-            console.log('clicked')
             document.getElementById('nav-check').checked = false
         }
     }
@@ -85,5 +116,38 @@ function addHide(slides, current, count) {
         } else {
             slides[i].classList.remove('hide')
         }
+    }
+}
+
+function fetchPage(path, callback) {
+    fetch(path)
+        .then(response => {
+            return response.text()
+        })
+        .then(data => {
+            callback(data)
+        });
+}
+
+function getData(path, callback) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4) {
+            if (httpRequest.status === 200) {
+                var data = JSON.parse(httpRequest.responseText);
+                if (callback) callback(data);
+            }
+        }
+    };
+    httpRequest.open('GET', path);
+    httpRequest.send(); 
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
